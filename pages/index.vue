d133f5aff838ffb34a15df1973a3a797b74efe7a
Emmanuel687 <koechemmanuel2002@gmail.com>
2024-09-25 21:12:12 +0300

    Initial Commit

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
 <script setup></script>
 <template>
   <h1 class="text-3xl font-bold underline">
-    Hello world!!
+    Hello world!!!
   </h1>
   <AutoComplete v-model="value" :suggestions="items" @complete="search" />
 </template>
